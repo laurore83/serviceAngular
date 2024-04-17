16be81bbf9a543179369a9bf0351e974a9b15651
Laurore83 <laurore83@yahoo.fr>
2024-04-17 16:33:00 +0200

    test done

--- src/app/cocktail.service.spec.ts
+++ src/app/cocktail.service.spec.ts
@@ -1,16 +1,47 @@
-import { TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { CocktailListComponent } from './cocktail-list/cocktail-list.component';
 import { CocktailService } from './cocktail.service';
+import { HttpClientModule } from '@angular/common/http';
 
-describe('CocktailService', () => {
-  let service: CocktailService;
+describe('CocktailListComponent', () => {
+  let component: CocktailListComponent;
+  let fixture: ComponentFixture<CocktailListComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CocktailService);
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CocktailListComponent],
+      imports: [HttpClientModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CocktailListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should be created', () => {
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('service should be created', () => {
+    const service: CocktailService = TestBed.inject(CocktailService);
     expect(service).toBeTruthy();
   });
+
+  it('service should have a "getCocktails" method which returns at least one object', () => {
+    const service: CocktailService = TestBed.inject(CocktailService);
+    const cocktails = service.getCocktails();
+    expect(cocktails.length).toBeGreaterThan(0);
+  });
+
+  it('should create a CocktailListComponent instance', waitForAsync(() => {
+    const fixture = TestBed.createComponent(CocktailListComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+
+  it('component should have a public property named "cocktails"', waitForAsync(() => {
+    const fixture = TestBed.createComponent(CocktailListComponent);
+    fixture.detectChanges();
+    expect(fixture.componentInstance.cocktails).toBeTruthy();
+  }));
 });
